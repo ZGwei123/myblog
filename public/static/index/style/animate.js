419ff548ed8e8a3de880b8f37c1accd9481f2524
ZGwei123 <839319250@qq.com>
2017-11-16 20:53:10 +0800

    添加一些资源文件和扩展一些功能代码

---
+++ public/static/index/style/animate.js
@@ -0,0 +1,93 @@
+﻿// JavaScript Document
+
+	//initial time
+	var h_current = -1;
+	var m1_current = -1;
+	var m2_current = -1;
+	var s1_current = -1;
+	var s2_current= -1;
+
+	
+	function flip (upperId, lowerId, changeNumber, pathUpper, pathLower){
+		var upperBackId = upperId+"Back";
+		$(upperId).src = $(upperBackId).src;
+		$(upperId).setStyle("height", "40px");
+		$(upperId).setStyle("visibility", "visible");
+		$(upperBackId).src = pathUpper+parseInt(changeNumber)+".png";
+		
+		$(lowerId).src = pathLower+parseInt(changeNumber)+".png";
+		$(lowerId).setStyle("height", "0px");
+		$(lowerId).setStyle("visibility", "visible");
+		
+		var flipUpper = new Fx.Tween(upperId, {duration: 200, transition: Fx.Transitions.Sine.easeInOut});
+		flipUpper.addEvents({
+			'complete': function(){
+				var flipLower = new Fx.Tween(lowerId, {duration: 200, transition: Fx.Transitions.Sine.easeInOut});
+					flipLower.addEvents({
+						'complete': function(){	
+							lowerBackId = lowerId+"Back";
+							$(lowerBackId).src = $(lowerId).src;
+							$(lowerId).setStyle("visibility", "hidden");
+							$(upperId).setStyle("visibility", "hidden");
+						}				});					
+					flipLower.start('height', 40);
+					
+			}
+							});
+		flipUpper.start('height', 0);
+		
+		
+	}//flip
+				
+	
+	function retroClock(){
+		
+		// get new time
+		 now = new Date();
+		 h = now.getHours();
+		 m1 = now.getMinutes() / 10;
+		 m2 = now.getMinutes() % 10;
+		 s1 = now.getSeconds() / 10;
+		 s2 = now.getSeconds() % 10;
+		 if(h < 12)
+		 	ap = "AM";
+		 else{ 
+		 	if( h == 12 )
+				ap = "PM";
+			else{
+				ap = "PM";
+				h -= 12; }
+		 }
+		 
+		 //change pads
+		 
+		 if( h != h_current){
+			flip('hoursUp', 'hoursDown', h, 'http://localhost/tp/public/static/index/images/Single/Up/'+ap+'/', 'http://localhost/tp/public/static/index/images/Single/Down/'+ap+'/');
+			h_current = h;
+		}
+		
+		if( m2 != m2_current){
+			flip('minutesUpRight', 'minutesDownRight', m2, 'http://localhost/tp/public/static/index/images/Double/Up/Right/', 'http://localhost/tp/public/static/index/images/Double/Down/Right/');
+			m2_current = m2;
+			
+			flip('minutesUpLeft', 'minutesDownLeft', m1, 'http://localhost/tp/public/static/index/images/Double/Up/Left/', 'http://localhost/tp/public/static/index/images/Double/Down/Left/');
+			m1_current = m1;
+		}
+		
+		 if (s2 != s2_current){
+			flip('secondsUpRight', 'secondsDownRight', s2, 'http://localhost/tp/public/static/index/images/Double/Up/Right/', 'http://localhost/tp/public/static/index/images/Double/Down/Right/');
+			s2_current = s2;
+			
+			flip('secondsUpLeft', 'secondsDownLeft', s1, 'http://localhost/tp/public/static/index/images/Double/Up/Left/', 'http://localhost/tp/public/static/index/images/Double/Down/Left/');
+			s1_current = s1;
+		}
+		
+		
+		
+			
+		
+	}
+	
+	setInterval('retroClock()', 1000);
+			
+	
---
+++ public/static/index/style/silder.js
@@ -0,0 +1,10 @@
+/*µ¼º½µ±Ç°Ò³¸ßÁÁ*/
+var obj=null;
+var As=document.getElementById('topnav').getElementsByTagName('a');
+obj = As[0];
+for(i=1;i<As.length;i++){if(window.location.href.indexOf(As[i].href)>=0)
+obj=As[i];}
+obj.id='topnav_current'
+
+/*°Ù¶È·ÖÏí¹ã¸æ*/
+window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdMiniList":false,"bdPic":"","bdStyle":"0","bdSize":"32"},"share":{}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];
